d0965cf0c03d19434eb3652c5f6da2ff57bb973f
Double <double0101@foxmail.com>
2019-01-31 17:35:36 +0800

    first commit

---
+++ src/main/java/com/double0101/learning/App.java
@@ -0,0 +1,13 @@
+package com.double0101.learning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
